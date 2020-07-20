5c76bc27a1aefc300413dadd4acec6848dfc3e9f
Alex Jackson <alex.lee.jackson@gmail.com>
2020-07-20 16:00:26 -0400

    initial commit

---
+++ mafia/mafiaNewGame.py
@@ -0,0 +1,45 @@
+import json
+from dataRepos import GameStateRepo
+
+def new_game(gameId):
+    repo = GameStateRepo()
+    
+    state = repo.CreateNewGame(gameId)
+    
+    return state
+
+def generate_new_game_state():
+    return {
+        '_id' : 'a',
+        'state': 'marshalling',
+        'players': []
+    }
+
+def lambda_handler(event, context):
+    try:
+        game = new_game(event['game_id'])
+        print(game)
+        state = None
+        if game == None:
+            message = json.dumps('Game already exists.')
+        else:
+            message = json.dumps('Marshalling new game')
+            state = game['state']
+        response = {
+            'statusCode': 200,
+            'body': { 
+                'message': message,
+                'game_id': event['game_id'],
+                'game_state': state
+            }
+        }
+    except Exception as e:
+        print(e)
+        response = {
+            'statusCode': 500,
+            'body': json.dumps('Error creating game!')
+        }
+    return response
+
+if __name__ == "__main__":
+    print(new_game(None))

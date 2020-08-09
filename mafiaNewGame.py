59af32bb6588e7d86a85f78535dd44e6cdd77eae
Alex Jackson <alex.lee.jackson@gmail.com>
2020-08-09 12:58:39 -0400

    correct apigateway proxy response

--- mafiaNewGame.py
+++ mafiaNewGame.py
@@ -1,6 +1,7 @@
 import json
 from data_access.dataRepos import GameStateRepo
 
+
 def new_game(gameId):
     repo = GameStateRepo()
     
@@ -9,29 +10,42 @@ def new_game(gameId):
     return state
 
 def lambda_handler(event, context):
-    try:
-        id = event['game_id']
-        game = new_game(id)
-        state = None
-        if game == None:
-            message = json.dumps('Game already exists.')
-        else:
-            message = json.dumps('Marshalling new game')
-        response = {
-            'statusCode': 200,
-            'body': { 
-                'message': message,
-                'game_id': id,
-                'game_state': state
-            }
-        }
-    except Exception as e:
-        print(e)
-        response = {
-            'statusCode': 500,
-            'body': json.dumps('Error creating game!')
-        }
+    print(event)
+    print(context)
+    #try:
+    id = extractIdParameter(event)
+    game = new_game(id)
+    state = None
+    if game == None:
+        message = json.dumps('Game already exists.')
+    else:
+        message = json.dumps('Marshalling new game')
+    response = {
+        'statusCode': 200,
+        'headers' : {},
+        'body': json.dumps({ 
+            'message': message,
+            'game_id': id,
+            'game_state': state
+        }),
+        'isBase64Encoded' : False
+    }
+    # except Exception as e:
+    #     print(e)
+    #     response = {
+    #         'statusCode': 500,
+    #         'body': json.dumps('Error creating game!')
+    #     }
     return response
+
+def extractIdParameter(event):
+    if 'game_id' in event:
+        return event['game_id']
+    elif 'pathParameters' in event:
+        if 'game_id' in event['pathParameters']:
+            return event['pathParameters']['game_id']
+    else:
+        raise ValueError('Missing game_id parameter')
 
 if __name__ == "__main__":
     print(new_game(None))
--- mafiaUpdateGame.py
+++ mafiaUpdateGame.py
@@ -3,32 +3,61 @@ from data_access.dataRepos import GameStateRepo
 from stateManagers.gameStateManager import GameStateManager
 
 def lambda_handler(event, context):
-    try:
-        game_id = event['game_id']
-        player_id = event['player_id']
-        action = event['action']
+    #try:
+    game_id, action, player_id = extractParameters(event)
+    gameRepo = GameStateRepo()
 
-        gameRepo = GameStateRepo()
+    gameState = gameRepo.GetGameState(game_id)
+    if gameState == None:
+        return None
+    manager = GameStateManager(gameState)
+    manager.transition(action,player_id)
 
-        gameState = gameRepo.GetGameState(game_id)
-        if gameState == None:
-            return None
-        manager = GameStateManager(gameState)
-        manager.transition(action,player_id)
-
-        gameRepo.UpdateGame(gameState)
-        response = {
-            'statusCode': 200,
-            'body': json.dumps('Game state updated'),
+    gameRepo.UpdateGame(gameState)
+    response = {
+        'statusCode': 200,
+        'headers' : {},
+        'body': json.dumps({
+            'message' : json.dumps('Game state updated'),
             'game': gameRepo._serializeGame(gameState)
-        }
-    except Exception as e:
-        print(e)
-        response = {
-            'statusCode': 500,
-            'body': json.dumps('Error updating game!')
-        }
+            }),
+        'isBase64Encoded' : False
+    }
+    # except Exception as e:
+    #     print(e)
+    #     response = {
+    #         'statusCode': 500,
+    #         'headers' : {},
+    #         'body': json.dumps('Error updating game!'),
+    #         'isBase64Encoded' : False
+    #     }
     return response
+
+def extractParameters(event):
+    if 'game_id' in event:
+        game_id = event['game_id']
+    elif 'pathParameters' in event:
+        if 'game_id' in event['pathParameters']:
+            game_id = event['pathParameters']['game_id']
+    else:
+        raise ValueError('Missing game_id parameter')
+
+    if 'action' in event:
+        action = event['action']
+    elif 'pathParameters' in event:
+        if 'action' in event['pathParameters']:
+            action = event['pathParameters']['action']
+    else:
+        raise ValueError('Missing action parameter')
+
+    player_id = None
+    if 'player_id' in event:
+        player_id = event['player_id']
+    elif 'pathParameters' in event:
+        if 'player_id' in event['pathParameters']:
+            player_id = event['pathParameters']['player_id']
+
+    return game_id, action, player_id
 
 if __name__ == "__main__":
     p_id = 'def'

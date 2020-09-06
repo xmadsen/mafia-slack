78596c42343fc71de5904b6443f95dd7e1309f36
Alex Jackson <alex.lee.jackson@gmail.com>
2020-09-06 14:01:09 -0400

    payload parser unit tests

---
+++ tests/unit/util/test_slackParser.py
@@ -0,0 +1,10 @@
+from util.slack_payload_parser import parse_payload
+
+def test_convertDelimitedParamsIntoDict():
+    raw_args = 'token=testToken&team_id=testId&team_domain=testDomain&channel_id=testChannel&channel_name=privategroup'
+    parsed_args = parse_payload(raw_args)
+    assert parsed_args['token'] == 'testToken'
+    assert parsed_args['team_id'] == 'testId'
+    assert parsed_args['team_domain'] == 'testDomain'
+    assert parsed_args['channel_id'] == 'testChannel'
+    assert parsed_args['channel_name'] == 'privategroup'

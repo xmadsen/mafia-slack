fc973e36a898d64b216916cbde2c75e11603e0f2
xmadsen <xander.madsen@gmail.com>
2020-11-18 15:01:02 -0500

    update setup.py git commit -m update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='mafia-slack',
     version='1.0',
     description='A game of Mafia for Slack.',
-    install_requires=['boto3', 'slackclient']
+    install_requires=['boto3', 'slackclient'],
     packages=find_packages(where='.'),
     package_dir={
         '': '.',

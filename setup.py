773cb343960a0ea892cef8bff02f0f406e53c99e
xmadsen <xander.madsen@gmail.com>
2020-11-19 15:46:15 -0500

    Add setup.py - required for the GH actions to run properly

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mafia-slack',
+    version='1.0',
+    description='A game of Mafia for Slack.',
+    install_requires=['boto3', 'slackclient'],
+    packages=find_packages(where='.'),
+    package_dir={
+        '': '.',
+    },
+)

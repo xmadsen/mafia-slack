53df1780460392a56f174a03bc4bb7bd50183f77
xmadsen <xmadsen@users.noreply.github.com>
2020-11-24 20:25:33 +0000

    autopep8 action fixes

--- stateManagers/nightStateManager.py
+++ stateManagers/nightStateManager.py
@@ -7,7 +7,7 @@ class NightStateManager(GameStateManager):
     def _transitionFromState(self, action, data, executor):
         if action == Actions.MURDER:
             toMurder = self.gameState.findPlayerWithId(data)
-            if toMurder == None or toMurder.role == Roles.MAFIA:
+            if toMurder is None or toMurder.role == Roles.MAFIA:
                 return False
             murderer = self.gameState.findPlayerWithId(executor)
             if murderer.role != Roles.MAFIA:
@@ -16,7 +16,8 @@ class NightStateManager(GameStateManager):
             mafiaMembers = self.gameState.findPlayersWithRole(Roles.MAFIA)
             livingMafia = [
                 m for m in mafiaMembers if m.state == PlayerStates.ALIVE]
-            if len([m for m in mafiaMembers if m.vote == toMurder.id]) == len(livingMafia):
+            if len([m for m in mafiaMembers if m.vote ==
+                    toMurder.id]) == len(livingMafia):
                 toMurder.state = PlayerStates.DEAD
                 if self._isGameOver():
                     self.gameState.state = GameStates.GAME_OVER
